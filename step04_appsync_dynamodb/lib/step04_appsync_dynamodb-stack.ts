125e3b303c9c90faf6cce233cfb367f1f395058b
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-10-26 21:29:04 +0500

    Create new App sync Api

--- step04_appsync_dynamodb/lib/step04_appsync_dynamodb-stack.ts
+++ step04_appsync_dynamodb/lib/step04_appsync_dynamodb-stack.ts
@@ -1,9 +1,31 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import {
+  Duration,
+  Expiration,
+} from "@aws-cdk/aws-appsync/node_modules/@aws-cdk/core";
 
 export class Step04AppsyncDynamodbStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // AppSync api to genrate an api
+    const api = new appsync.GraphqlApi(this, "TodoApi", {
+      name: "todos-appsync-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.gql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: Expiration.after(Duration.days(20)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    });
   }
 }
